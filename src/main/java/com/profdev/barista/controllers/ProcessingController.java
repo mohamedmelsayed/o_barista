1c6d63dee37073a36a41bff3b1630ed12728b1a5
Mohamed <mohamedosmanelsayed@yahoo.com>
2022-06-24 22:32:16 +0200

    barista update allow cors origin request

--- src/main/java/com/profdev/barista/controllers/ProcessingController.java
+++ src/main/java/com/profdev/barista/controllers/ProcessingController.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,19 +23,29 @@ public class ProcessingController {
 
 	@Autowired
 	private ServicesRepository repository;
-
+	@CrossOrigin(origins = "http://localhost:1222")
 	@GetMapping(value = { "", "/" })
-	public List<OrderProcessing> getAll() {
-		return (List<OrderProcessing>) repository.findAll();
+	public Iterable<OrderProcessing> getAll() {
+		return  repository.findAll();
 	}
-
+	@CrossOrigin(origins = "http://localhost:1222")
 	@PostMapping
 	public OrderProcessing insert(@RequestBody OrderProcessing request) {
 		return repository.save(request);
 	}
+	@CrossOrigin(origins = "http://localhost:1222")
 	@RequestMapping(value="{id}",method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public OrderProcessing update(@RequestBody OrderProcessing request,@PathVariable long id) {
 		return repository.save(request);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:1222")
+	@RequestMapping(value="{id}",method = RequestMethod.DELETE)
+	@ResponseBody
+	public boolean delete(@PathVariable long id) {
+		OrderProcessing processing=repository.findById(id).get();
+		 repository.delete(processing);
+		 return true;
 	}
 }
